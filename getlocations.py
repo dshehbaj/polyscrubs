92e414b04c83e4a9928df6ab954109cd9f20d3bf
dshehbaj <shehbaj.dhillon@gmail.com>
2021-08-04 12:53:57 -0700

    Able to generate links for all the buildings.

---
+++ getlocations.py
@@ -0,0 +1,30 @@
+from requests import get
+from bs4 import BeautifulSoup
+from time import sleep
+
+main_url = "http://washalert.washlaundry.com/washalertweb/calpoly/"
+
+links = {
+    "Cerro Vista" : "cerro-vista.html",
+    "North Mountain" : "north-mountain.html",
+    "PCV" : "poly-canyon-village.html",
+    "Sierra Madre" : "sierra-madre-towers.html",
+    "Red Bricks" : "south-mountain.html",
+    "YTT" : "ytt.html",
+    "Yosemite" : "yosemite.html"
+    }
+
+def getLocations():
+    locations = {}
+    for key, value in links.items():
+        locations[key] = {}
+        data = get(main_url + value)
+        soup = BeautifulSoup(data.text, "html.parser")
+        for link in soup.find_all("a"):
+            tokens = link.get("href").split("=")
+            if len(tokens) == 2:
+                locations[key][link.string] = tokens[1]
+    return locations
+
+if __name__ == "__main__":
+    getLocations()
---
+++ links.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests as req
+import getlocations as gl
+
+keys = ["CerroVista", "NorthMountain", "PCV", "SierraMadre", "RedBricks", "YTT", "Yosemite"]
+wash_alert = "http://washalert.washlaundry.com/washalertweb/calpoly/WASHALERtweb.aspx?location="
+
+
+def main():
+    locations = gl.getLocations()
+    for location in locations.keys():
+        for building in locations[location].keys():
+            url = wash_alert + locations[location][building]
+            print(url + "    " + location  + " "+ building.strip())
+
+if __name__ == "__main__":
+    main()
