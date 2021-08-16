41195db5d420f47ada5863e6a5a94a3dd7895375
dshehbaj <shehbaj.dhillon@gmail.com>
2021-08-15 17:54:14 -0700

    Test works.

--- scraper/links.py
+++ scraper/links.py
@@ -1,20 +1,26 @@
 from bs4 import BeautifulSoup
+from time import sleep
 import requests as req
-import getlocations as gl
+import getLocations as gl
+import getMachines as gm
 
 wash_alert = "http://washalert.washlaundry.com/washalertweb/calpoly/WASHALERtweb.aspx?location="
-machines = {}
 
-def main():
+def getData():
+    machine_urls = {}
+    machine_data = {}
     locations = gl.getLocations()
     for location in locations.keys():
         for building in locations[location].keys():
             url = wash_alert + locations[location][building]
             key = location.lower().strip() + ' ' + building.lower().strip()
             key = key.replace(' ', '-')
-            key = key.replace('---', '-') #For Yakitutu Buildings
-            machines[key] = url
-    print(machines)
+            key = key.replace('---', '-') #Needed For Yakitutu Buildings
+            machine_urls[key] = url
+    for location, locUrl in machine_urls.items():
+        machine_data[location] = gm.getMachines(locUrl)
+    return machine_data
 
 if __name__ == "__main__":
-    main()
+    data = getData()
+    print(data)
