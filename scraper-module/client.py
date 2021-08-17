1893d5d34b622348cf8a2f00b322f9ef0248c0b4
dshehbaj <shehbaj.dhillon@gmail.com>
2021-08-16 23:50:53 -0700

    Cron Job works.:

--- scraper-module/client.py
+++ scraper-module/client.py
@@ -2,10 +2,15 @@ import boto3
 from time import time
 import getRecords as gr
 import os
+import secrets
 
 def handler(event, context):
-    DB_NAME = os.environ['DB_NAME']
-    TBL_NAME = os.environ['TBL_NAME']
+    if (event and context):
+        DB_NAME = os.environ['DB_NAME']
+        TBL_NAME = os.environ['TBL_NAME']
+    else:
+        DB_NAME = secrets.DB_NAME
+        TBL_NAME = secrets.TBL_NAME
     client = boto3.client('timestream-write')
     data = gr.getData()
     for records in data.values():
@@ -15,4 +20,7 @@ def handler(event, context):
             Records=records
         )
     return data
+
+if __name__ == "__main__":
+    handler(None, None)
 
