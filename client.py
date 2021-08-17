811f5b63eb99f2079b69fa65fe04913ceb036ffc
dshehbaj <shehbaj.dhillon@gmail.com>
2021-08-16 19:54:00 -0700

    Testing done.

--- client.py
+++ client.py
@@ -1,10 +1,20 @@
 import boto3
 from time import time
-import scraper.getRecords as gr
+import getRecords as gr
+import secrets
 
 def main():
-    return gr.getData()
+    DB_NAME = secrets.DB_NAME
+    TBL_NAME = secrets.TBL_NAME
+    client = boto3.client('timestream-write')
+    data = gr.getData()
+    for records in data.values():
+        client.write_records(
+            DatabaseName=DB_NAME,
+            TableName=TBL_NAME,
+            Records=records
+        )
+    return data
 
 if __name__ == "__main__":
-    data = main()
-    print(data)
+    main()
