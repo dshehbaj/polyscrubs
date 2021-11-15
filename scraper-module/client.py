4907a14da8519e1f46650e946dac6e1d287ded22
dshehbaj <shehbaj.dhillon@gmail.com>
2021-11-14 16:41:35 -0800

    Removed sls code.

--- scraper-module/client.py
+++ scraper-module/client.py
@@ -1,17 +1,10 @@
 import boto3
 import getRecords as gr
-import os
 import secrets
 
-def handler(event, context):
-    if (event and context):
-        DB_NAME = os.environ['DB_NAME']
-        TBL_NAME = os.environ['TBL_NAME']
-    else:
-        DB_NAME = secrets.DB_NAME
-        TBL_NAME = secrets.TBL_NAME
-        print(DB_NAME)
-        print(TBL_NAME)
+def handler():
+    DB_NAME = secrets.DB_NAME
+    TBL_NAME = secrets.TBL_NAME
 
     #Choses default aws cli profile, change profile_name to use a different profile
     client = boto3.Session(profile_name="default").client('timestream-write')
